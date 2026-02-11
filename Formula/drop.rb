f35f23b6150dd7472c16b9e2a0575f541798918e
husseymarcos <mhussey@mail.austral.edu.ar>
2026-02-11 11:28:53 -0300

    chore: update Drop formula with new URL, SHA256, and version for improved package management

--- Formula/drop.rb
+++ Formula/drop.rb
@@ -1,6 +1,9 @@
 class Drop < Formula
   desc "Ephemeral high-performance server to share files over the local network"
   homepage "https://github.com/husseymarcos/drop"
+  url "https://github.com/husseymarcos/drop/archive/refs/heads/main.tar.gz"
+  sha256 "488409fb031b32e1d5569a17e6ca2d00e093edd4777ccfc238df33215ee36dc3"
+  version "main"
   head "https://github.com/husseymarcos/drop.git", branch: "main"
 
   depends_on "bun"
